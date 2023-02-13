e1229154374002c3047c6422a539cd15029867b1
Rabia Yousuf <53699291+rabiayousuf187@users.noreply.github.com>
2023-02-13 15:51:10 +0500

    Slider 3 completed.

--- js/script.js
+++ js/script.js
@@ -1,38 +1,4 @@
-
-// var swiper = new Swiper(".mySwiper", {
-//     slidesPerView: 1,
-//     spaceBetween: 10,
-//     pagination: {
-//     el: ".swiper-pagination",
-//     clickable: true,
-//     },
-//     navigation: {
-//         nextEl: ".swiper-button-next",
-//         prevEl: ".swiper-button-prev",
-//       },
-//     breakpoints: {
-//     "@0.00": {
-//         slidesPerView: 1,
-//         spaceBetween: 10,
-//     },
-//     "@0.75": {
-//         slidesPerView: 2,
-//         spaceBetween: 20,
-//     },
-//     "@1.00": {
-//         slidesPerView: 3,
-//         spaceBetween: 40,
-//     },
-//     "@1.50": {
-//         slidesPerView: 4,
-//         spaceBetween: 50,
-//     },
-//     },
-// });
-
-
 // <!-- Initialize Swiper -->
-
 var swiper = new Swiper('#resize1', {
     slidesPerView: 3,
     direction: getDirection(),
@@ -54,8 +20,9 @@ function getDirection() {
     return direction;
 }
 
+// Slider 2
 var swiper = new Swiper('#resize2', {
-    slidesPerView: 5,
+    slidesPerView: 4,
     direction: getDirection(),
     navigation: {
     nextEl: '.swiper-button-next2',
@@ -74,3 +41,26 @@ function getDirection() {
 
     return direction;
 }
+
+// Slider 2
+var swiper = new Swiper('#resize3', {
+    slidesPerView: 4,
+    direction: getDirection(),
+    navigation: {
+    nextEl: '.swiper-button-next2',
+    prevEl: '.swiper-button-prev2',
+    },
+    on: {
+    resize: function () {
+        swiper.changeDirection(getDirection());
+    },
+    },
+});
+
+function getDirection() {
+    var windowWidth = window.innerWidth;
+    var direction = window.innerWidth <= 760 ? 'vertical' : 'horizontal';
+
+    return direction;
+}
+
