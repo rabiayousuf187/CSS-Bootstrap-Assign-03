d148eeda8ae771a8cebdc136624977f787e72d11
Rabia Yousuf <53699291+rabiayousuf187@users.noreply.github.com>
2023-02-13 06:50:00 +0500

    product-slider-create and test

--- js/script.js
+++ js/script.js
@@ -1,26 +1,76 @@
+
 // var swiper = new Swiper(".mySwiper", {
 //     slidesPerView: 1,
 //     spaceBetween: 10,
 //     pagination: {
-//       el: ".swiper-pagination",
-//       clickable: true,
+//     el: ".swiper-pagination",
+//     clickable: true,
 //     },
+//     navigation: {
+//         nextEl: ".swiper-button-next",
+//         prevEl: ".swiper-button-prev",
+//       },
 //     breakpoints: {
-//       "@0.00": {
+//     "@0.00": {
 //         slidesPerView: 1,
 //         spaceBetween: 10,
-//       },
-//       "@0.75": {
+//     },
+//     "@0.75": {
 //         slidesPerView: 2,
 //         spaceBetween: 20,
-//       },
-//       "@1.00": {
+//     },
+//     "@1.00": {
 //         slidesPerView: 3,
 //         spaceBetween: 40,
-//       },
-//       "@1.50": {
+//     },
+//     "@1.50": {
 //         slidesPerView: 4,
 //         spaceBetween: 50,
-//       },
 //     },
-//   });
+//     },
+// });
+
+
+// <!-- Initialize Swiper -->
+
+var swiper = new Swiper('#resize1', {
+    slidesPerView: 3,
+    direction: getDirection(),
+    navigation: {
+    nextEl: '.swiper-button-next2',
+    prevEl: '.swiper-button-prev2',
+    },
+    on: {
+    resize: function () {
+        swiper.changeDirection(getDirection());
+    },
+    },
+});
+
+function getDirection() {
+    var windowWidth = window.innerWidth;
+    var direction = window.innerWidth <= 760 ? 'vertical' : 'horizontal';
+
+    return direction;
+}
+
+var swiper = new Swiper('#resize2', {
+    slidesPerView: 5,
+    direction: getDirection(),
+    navigation: {
+    nextEl: '.swiper-button-next2',
+    prevEl: '.swiper-button-prev2',
+    },
+    on: {
+    resize: function () {
+        swiper.changeDirection(getDirection());
+    },
+    },
+});
+
+function getDirection() {
+    var windowWidth = window.innerWidth;
+    var direction = window.innerWidth <= 760 ? 'vertical' : 'horizontal';
+
+    return direction;
+}
