0a18135deb2049b5c03212b10784983e35119540
Rabia Yousuf <53699291+rabiayousuf187@users.noreply.github.com>
2023-02-15 00:05:34 +0500

    completed now

--- js/script.js
+++ js/script.js
@@ -1,26 +1,152 @@
-// var swiper = new Swiper(".mySwiper", {
-//     slidesPerView: 1,
-//     spaceBetween: 10,
-//     pagination: {
-//       el: ".swiper-pagination",
-//       clickable: true,
-//     },
-//     breakpoints: {
-//       "@0.00": {
-//         slidesPerView: 1,
-//         spaceBetween: 10,
-//       },
-//       "@0.75": {
-//         slidesPerView: 2,
-//         spaceBetween: 20,
-//       },
-//       "@1.00": {
-//         slidesPerView: 3,
-//         spaceBetween: 40,
-//       },
-//       "@1.50": {
-//         slidesPerView: 4,
-//         spaceBetween: 50,
-//       },
-//     },
-//   });
+// <!-- Initialize Swiper -->
+var swiper = new Swiper('#resize1', {
+    slidesPerView: 3,
+    direction: getDirection(),
+    navigation: {
+    nextEl: '.swiper-button-next2',
+    prevEl: '.swiper-button-prev2',
+    },
+    on: {
+    resize: function () {
+        swiper.changeDirection(getDirection());
+    },
+    },
+});
+
+function getDirection() {
+    var windowWidth = window.innerWidth;
+    var direction = window.innerWidth <= 760 ? 'vertical' : 'horizontal';
+
+    return direction;
+}
+
+// Slider 2
+var swiper = new Swiper('#resize2', {
+    slidesPerView: 4,
+    direction: getDirection(),
+    navigation: {
+    nextEl: '.swiper-button-next2',
+    prevEl: '.swiper-button-prev2',
+    },
+    on: {
+    resize: function () {
+        swiper.changeDirection(getDirection());
+    },
+    },
+});
+
+function getDirection() {
+    var windowWidth = window.innerWidth;
+    var direction = window.innerWidth <= 760 ? 'vertical' : 'horizontal';
+
+    return direction;
+}
+
+// Slider 2
+var swiper = new Swiper('#resize3', {
+    slidesPerView: 4,
+    direction: getDirection(),
+    navigation: {
+    nextEl: '.swiper-button-next2',
+    prevEl: '.swiper-button-prev2',
+    },
+    on: {
+    resize: function () {
+        swiper.changeDirection(getDirection());
+    },
+    },
+});
+
+function getDirection() {
+    var windowWidth = window.innerWidth;
+    var direction = window.innerWidth <= 760 ? 'vertical' : 'horizontal';
+
+    return direction;
+}
+// Slider
+var swiper = new Swiper('#resize4', {
+    slidesPerView: 4,
+    direction: getDirection(),
+    navigation: {
+    nextEl: '.swiper-button-next2',
+    prevEl: '.swiper-button-prev2',
+    },
+    on: {
+    resize: function () {
+        swiper.changeDirection(getDirection());
+    },
+    },
+});
+
+function getDirection() {
+    var windowWidth = window.innerWidth;
+    var direction = window.innerWidth <= 760 ? 'vertical' : 'horizontal';
+
+    return direction;
+}
+
+// Slider
+var swiper = new Swiper('#resize5', {
+    slidesPerView: 4,
+    direction: getDirection(),
+    navigation: {
+    nextEl: '.swiper-button-next2',
+    prevEl: '.swiper-button-prev2',
+    },
+    on: {
+    resize: function () {
+        swiper.changeDirection(getDirection());
+    },
+    },
+});
+
+function getDirection() {
+    var windowWidth = window.innerWidth;
+    var direction = window.innerWidth <= 760 ? 'vertical' : 'horizontal';
+
+    return direction;
+}
+// Slider
+var swiper = new Swiper('#resize6', {
+    slidesPerView: 3,
+    direction: getDirection(),
+    navigation: {
+    nextEl: '.swiper-button-next2',
+    prevEl: '.swiper-button-prev2',
+    },
+    on: {
+    resize: function () {
+        swiper.changeDirection(getDirection());
+    },
+    },
+});
+
+function getDirection() {
+    var windowWidth = window.innerWidth;
+    var direction = window.innerWidth <= 760 ? 'vertical' : 'horizontal';
+
+    return direction;
+}
+
+// Slider
+var swiper = new Swiper('#resize7', {
+    slidesPerView: 3,
+    direction: getDirection(),
+    navigation: {
+    nextEl: '.swiper-button-next2',
+    prevEl: '.swiper-button-prev2',
+    },
+    on: {
+    resize: function () {
+        swiper.changeDirection(getDirection());
+    },
+    },
+});
+
+function getDirection() {
+    var windowWidth = window.innerWidth;
+    var direction = window.innerWidth <= 760 ? 'vertical' : 'horizontal';
+
+    return direction;
+}
+
